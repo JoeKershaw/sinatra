a200ea54f02c9a3236b6f822fd52c33ec0df36fd
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-26 16:21:33 +0000

    added a balla border

--- app.rb
+++ app.rb
@@ -12,4 +12,8 @@ get '/secret' do
   "Mushrooms are disgusting"
 end
 
+get '/cat' do
+  '<img src="http://bit.ly/1eze8aE" alt="Cute kitten" style="border:dotted red 35px">'
+end
+
 set :session_secret, 'super secret'
