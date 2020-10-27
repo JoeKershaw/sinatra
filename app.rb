1bc67cc7e25e7e247675ca2050670952e5fd0fb4
Joseph <jpkershaw17@gmail.com>
2020-10-27 14:40:28 +0000

    posted params

--- app.rb
+++ app.rb
@@ -12,8 +12,18 @@ get '/secret' do
   "Mushrooms are disgusting"
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
